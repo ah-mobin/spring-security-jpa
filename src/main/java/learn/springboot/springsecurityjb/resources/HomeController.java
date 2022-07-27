a241ed28805d961d31cd48e8221f655c98aa97bf
mobin-newroz <ahmobin@newroztech.com>
2022-07-27 13:14:51 +0600

    chore : spring security dependency add

--- src/main/java/learn/springboot/springsecurityjb/resources/HomeController.java
+++ src/main/java/learn/springboot/springsecurityjb/resources/HomeController.java
@@ -1,4 +1,13 @@
 package learn.springboot.springsecurityjb.resources;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HomeController {
+
+    @GetMapping("/")
+    public String home(){
+        return ("<h1>Welcome to spring boot application</h1>");
+    }
 }
