a809bc77f5ad2fce7a722b4be252c147e60f389e
mobin-newroz <ahmobin@newroztech.com>
2022-07-28 16:12:49 +0600

    feat : jdbc authentication

--- src/main/java/learn/springboot/springsecurityjb/config/SecurityConfiguration.java
+++ src/main/java/learn/springboot/springsecurityjb/config/SecurityConfiguration.java
@@ -1,29 +1,37 @@
 package learn.springboot.springsecurityjb.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.sql.DataSource;
 
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+    @Autowired public DataSource dataSource;
+    /**
+     * this method is concern about the user authentication
+     */
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         // set configuration on the auth object
-        auth.inMemoryAuthentication()
-                .withUser("blah")
-                .password("blah")
-                .roles("USER")
-                .and()
-                .withUser("foo")
-                .password("foo")
-                .roles("ADMIN");
-    }
 
+        //use jdbc authentication
+        jdbcAuthentication(auth);
+
+        //use in memory authentication
+        //inMemoryAuthentication(auth);
+    }
+    /**
+     * this method is concern about the user authorization
+     */
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
@@ -37,5 +45,34 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Bean
     public PasswordEncoder getPasswordEncoder(){
         return NoOpPasswordEncoder.getInstance();
+    }
+
+    private void jdbcAuthentication(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .withDefaultSchema()
+                .withUser(
+                        User.withUsername("mobin")
+                                .password("password")
+                                .roles("USER")
+                )
+                .withUser(
+                        User.withUsername("admin")
+                                .password("password")
+                                .roles("ADMIN")
+                )
+        ;
+    }
+
+
+    private void inMemoryAuthentication(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("blah")
+                .password("blah")
+                .roles("USER")
+                .and()
+                .withUser("foo")
+                .password("foo")
+                .roles("ADMIN");
     }
 }
